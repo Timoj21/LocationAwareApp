ed769233486c12de1a92c738f3bca46ba5d256b6
Timo <timojenkins21@gmail.com>
2021-01-18 23:15:57 +0100

    [ADD] a lot of stuff

--- Tag/app/src/main/java/com/example/tag/PlayFragment.java
+++ Tag/app/src/main/java/com/example/tag/PlayFragment.java
@@ -1,22 +1,35 @@
 package com.example.tag;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tag.gui.LoginActivity;
+import com.example.tag.gui.MainActivity;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 
 public class PlayFragment extends Fragment {
     private static final String TAG = PlayFragment.class.getSimpleName();
+
+    private AppCompatButton settingsButton;
+    private AppCompatButton centerButton;
+
+    private MainActivity mainActivity;
 
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
@@ -25,14 +38,69 @@ public class PlayFragment extends Fragment {
     private MyLocationNewOverlay locationOverlay;
     private MapController mapController;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestPermissions(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, REQUEST_PERMISSIONS_REQUEST_CODE);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_play, container, false);
 
+        this.mainActivity = (MainActivity) getContext();
 
+        this.settingsButton = view.findViewById(R.id.settingsButton);
+        this.centerButton = view.findViewById(R.id.centerButton);
+
+        this.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.toSettingsFragment();
+            }
+        });
+
+        this.centerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mapController.setCenter(locationOverlay.getMyLocation());
+                mapController.animateTo(locationOverlay.getMyLocation());
+                mapController.zoomTo(19);
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.mapView = getActivity().findViewById(R.id.mapView);
+        this.mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
+        this.mapView.setMultiTouchControls(true);
+        this.mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        this.locationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getActivity().getApplicationContext()), this.mapView);
+        this.locationOverlay.enableMyLocation();
+        this.locationOverlay.enableFollowLocation();
+        this.mapView.getOverlays().add(this.locationOverlay);
+        this.mapController = new MapController(this.mapView);
+        this.mapController.setCenter(locationOverlay.getMyLocation());
+        this.mapController.animateTo(locationOverlay.getMyLocation());
+        this.mapController.zoomTo(19);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //TODO if not given permission block app
     }
 }
--- Tag/app/src/main/java/com/example/tag/SettingsFragment.java
+++ Tag/app/src/main/java/com/example/tag/SettingsFragment.java
@@ -1,9 +1,12 @@
 package com.example.tag;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.provider.Settings;
@@ -11,15 +14,77 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tag.gui.LoginActivity;
+import com.example.tag.gui.MainActivity;
+
 
 public class SettingsFragment extends Fragment {
     private static final String TAG = SettingsFragment.class.getSimpleName();
+
+    private AppCompatButton backButton;
+    private AppCompatButton newGameButton;
+
+    private AppCompatButton yesButton;
+    private AppCompatButton noButton;
+
+    private MainActivity mainActivity;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        this.mainActivity = (MainActivity) getActivity();
+
+        this.backButton = view.findViewById(R.id.settingsBackButton);
+        this.newGameButton = view.findViewById(R.id.newGameButton);
+
+        this.dialogBuilder = new AlertDialog.Builder(getContext());
+
+        this.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.toPlayFragment();
+            }
+        });
+
+        this.newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewGamePopup();
+            }
+        });
+
         return view;
+    }
+
+    public void createNewGamePopup(){
+        View popup = getLayoutInflater().inflate(R.layout.popup_new_game, null);
+
+        this.yesButton = popup.findViewById(R.id.yesNewGameButton);
+        this.noButton = popup.findViewById(R.id.noNewGameButton);
+
+        this.yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+                getActivity().finish();
+            }
+        });
+
+        this.noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        dialogBuilder.setView(popup);
+        dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
